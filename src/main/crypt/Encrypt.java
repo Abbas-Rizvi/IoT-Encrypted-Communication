d0994cd48510b4864f7087dc92255b7bb80c8e10
Abbas Rizvi <abbas@abbasr.ca>
2024-03-25 01:26:55 -0400

    changed encryption to hybrid model

--- src/main/crypt/Encrypt.java
+++ src/main/crypt/Encrypt.java
@@ -1,40 +1,89 @@
 package crypt;
-
-import java.security.*;
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.spec.SecretKeySpec;
 
-public class Encrypt{
+import java.security.Key;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+
+public class Encrypt {
 
     private PrivateKey privateKey;
 
-    // Constructor to initialize private key
+    // initialize private key
     public Encrypt() {
         Keys keys = new Keys();
         privateKey = keys.getPrivatKey();
     }
 
-    // Encrypt message with given public key
+    // encrypt message with RSA and AES
     public byte[] encrypt(byte[] message, PublicKey publicKey) {
+
         try {
-            Cipher cipher = Cipher.getInstance("RSA");
-            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-            return cipher.doFinal(message);
+            // generate AES key
+            Key symmetricKey = generateSymmetricKey();
+
+            // encrypt message with AES 
+            Cipher aesCipher = Cipher.getInstance("AES");
+            aesCipher.init(Cipher.ENCRYPT_MODE, symmetricKey);
+            byte[] encryptedMessage = aesCipher.doFinal(message);
+
+            // encrypt key using RSA and public key
+            Cipher rsaCipher = Cipher.getInstance("RSA");
+            rsaCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            byte[] encryptedSymmetricKey = rsaCipher.doFinal(symmetricKey.getEncoded());
+
+            // combine key and message
+            byte[] merged = new byte[encryptedSymmetricKey.length + encryptedMessage.length];
+            System.arraycopy(encryptedSymmetricKey, 0, merged, 0, encryptedSymmetricKey.length);
+            System.arraycopy(encryptedMessage, 0, merged, encryptedSymmetricKey.length, encryptedMessage.length);
+
+            return merged;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
 
-    // Decrypt message with private key
+    // decrypt message with private key
     public byte[] decrypt(byte[] encryptedMessage) {
         try {
-            Cipher cipher = Cipher.getInstance("RSA");
-            cipher.init(Cipher.DECRYPT_MODE, privateKey);
-            return cipher.doFinal(encryptedMessage);
+            // extract symmetric key and message
+            int keySize = 256 / 8; 
+
+            byte[] encryptedSymmetricKey = new byte[keySize];
+            byte[] encryptedData = new byte[encryptedMessage.length - keySize];
+            System.arraycopy(encryptedMessage, 0, encryptedSymmetricKey, 0, keySize);
+            System.arraycopy(encryptedMessage, keySize, encryptedData, 0, encryptedData.length);
+
+            // decrypt symmetric key using RSA
+            Cipher rsaCipher = Cipher.getInstance("RSA");
+            rsaCipher.init(Cipher.DECRYPT_MODE, privateKey);
+            byte[] decryptedSymmetricKey = rsaCipher.doFinal(encryptedSymmetricKey);
+
+            // decrypt message using decrypted AES key
+            Key symmetricKey = new SecretKeySpec(decryptedSymmetricKey, "AES");
+            Cipher aesCipher = Cipher.getInstance("AES");
+            aesCipher.init(Cipher.DECRYPT_MODE, symmetricKey);
+
+            return aesCipher.doFinal(encryptedData);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
 
+    // generate AES key
+    private Key generateSymmetricKey() {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            keyGenerator.init(256); // AES key size is 256 bits
+            return keyGenerator.generateKey();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
